f307be6e9b33d02283c3afbddccc281a2303a26a
Tripura Sai <pulagamtripurasai@gmail.com>
2025-04-21 17:56:20 +0530

    Add files via upload

---
+++ project2.aspx
@@ -0,0 +1,132 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="project2.aspx.cs" Inherits="Project_1.project2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>SignUp Page</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-image: url(/Images/signin.jpg); /* put your image in the project folder */
+            background-size: cover;
+            background-position: center;
+            font-family: Arial;
+        }
+        .form-container {
+            background-color: rgba(255, 255, 255, 0.9);
+            padding: 30px;
+            margin: 50px auto;
+            width: 400px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px 2px #00000033;
+        }
+        .form-container h2 {
+            text-align: center;
+        }
+        .form-container table {
+            width: 100%;
+        }
+        .form-container input[type="text"],
+        .form-container input[type="password"],
+        .form-container input[type="date"] {
+            width: 100%;
+            padding: 8px;
+            margin: 5px 0;
+        }
+        .form-container input[type="submit"] {
+            padding: 8px 20px;
+            margin: 5px;
+        }
+        .gridview-container {
+            margin: 20px auto;
+            width: 90%;
+            background-color: #fff;
+            padding: 10px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="form-container">
+            <h2>SignUp Details</h2>
+            <table>
+                <tr>
+                    <td>UserName :</td>
+                    <td><asp:TextBox ID="txtname" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>PassWord :</td>
+                    <td><asp:TextBox ID="txtpwd" runat="server" TextMode="Password"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Email :</td>
+                    <td><asp:TextBox ID="txtemail" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Date of Birth :</td>
+                    <td><asp:TextBox ID="txtcld" runat="server" TextMode="Date"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td colspan="2" style="text-align:center">
+                        <asp:Button ID="btnsignin" runat="server" Text="Sign In" OnClick="btnsignin_Click" />
+                        <asp:Button ID="btnreset" runat="server" Text="Reset" OnClick="btnreset_Click" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="gridview-container" align="center">
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="sno" 
+                OnRowDeleting="GridView1_RowDeleting"
+                OnRowEditing="GridView1_RowEditing"
+                OnRowUpdating="GridView1_RowUpdating"
+                OnRowCancelingEdit="GridView1_RowCancelingEdit">
+                <Columns>
+                    
+                    
+                    <asp:TemplateField HeaderText="S.No">
+                    <ItemTemplate>
+                    <%# Container.DataItemIndex + 1 %>
+                    </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="User Name">
+                   <ItemTemplate>
+                   <%# Eval("UserName") %>
+                   </ItemTemplate>
+                   <EditItemTemplate>
+                   <asp:TextBox ID="txtUsername" runat="server" Text='<%# Bind("UserName") %>' />
+                   </EditItemTemplate>
+                   </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Password">
+                   <ItemTemplate>
+                   <%# Eval("Password") %>
+                   </ItemTemplate>
+                   <EditItemTemplate>
+                   <asp:TextBox ID="txtPassword" runat="server" Text='<%# Bind("Password") %>' />
+                   </EditItemTemplate>
+                   </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Email">
+                    <ItemTemplate>
+                   <%# Eval("Email") %>
+                   </ItemTemplate>
+                   <EditItemTemplate>
+                   <asp:TextBox ID="txtEmail" runat="server" Text='<%# Bind("Email") %>' />
+                   </EditItemTemplate>
+                   </asp:TemplateField>
+                    <asp:TemplateField HeaderText="DOB">
+                    <EditItemTemplate>
+                    <asp:TextBox ID="txtDOB" runat="server" Text='<%# Bind("DOB") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                    <%# Eval("DOB") %>
+                    </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:CommandField ShowEditButton="True" ShowDeleteButton="True" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ project2.aspx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_1
+{
+    public partial class project2 : System.Web.UI.Page
+    {
+            string connectionString = "server=.;Database=project1;Integrated Security=True";
+
+            protected void Page_Load(object sender, EventArgs e)
+            {
+                if (!IsPostBack)
+                {
+                    txtname.Focus();
+                    BindGrid();
+                }
+            }
+
+            protected void btnsignin_Click(object sender, EventArgs e)
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO signup (UserName, Password, Email, DOB) VALUES (@UserName, @Password, @Email, @DOB)";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@UserName", txtname.Text);
+                    cmd.Parameters.AddWithValue("@Password", txtpwd.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtemail.Text);
+                DateTime dob;
+                if (DateTime.TryParse(txtcld.Text, out dob))
+                {
+                    cmd.Parameters.AddWithValue("@DOB", dob);
+                }
+                con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                ClearFields();
+                BindGrid();
+            }
+
+            private void ClearFields()
+            {
+                txtname.Text = "";
+                txtpwd.Text = "";
+                txtemail.Text = "";
+                txtcld.Text = "";
+            }
+
+            private void BindGrid()
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM signup ORDER BY sno";
+                    SqlDataAdapter da = new SqlDataAdapter(query, con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+
+            protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+            {
+                int sno = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM signup WHERE sno = @sno";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@sno", sno);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+            ResequenceSno();
+            BindGrid();
+            }
+
+            private void ResequenceSno()
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(@"
+                    WITH CTE AS (
+                        SELECT sno, ROW_NUMBER() OVER (ORDER BY sno) AS rn
+                        FROM signup
+                    )
+                    UPDATE signup
+                   SET RowNumber = CTE.rn
+                   FROM signup
+                   INNER JOIN CTE ON signup.sno = CTE.sno", con);
+                    
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+            {
+                GridView1.EditIndex = e.NewEditIndex;
+                BindGrid();
+            }
+
+            protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+            {
+                GridView1.EditIndex = -1;
+                BindGrid();
+            }
+
+            protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+            {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            int sno = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+
+            string username = ((TextBox)row.FindControl("txtUsername")).Text;
+            string password = ((TextBox)row.FindControl("txtPassword")).Text;
+            string email = ((TextBox)row.FindControl("txtEmail")).Text;
+            string dob = ((TextBox)row.FindControl("txtDOB")).Text;
+
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE signup SET UserName=@UserName, Password=@Password, Email=@Email, DOB=@DOB WHERE sno=@sno";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@UserName", username);
+                    cmd.Parameters.AddWithValue("@Password", password);
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@DOB", Convert.ToDateTime(dob));
+                    cmd.Parameters.AddWithValue("@sno", sno);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                GridView1.EditIndex = -1;
+                BindGrid();
+            }
+
+            protected void btnreset_Click(object sender, EventArgs e)
+            {
+                ClearFields();
+            }
+
+        
+    }
+}
